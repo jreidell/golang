859f0e497d0632490240ba2b34831a00c491abd4
jreidell <joe@reidell.net>
2019-02-10 01:53:30 -0500

    More code and testing

--- lab/labcode.go
+++ lab/labcode.go
@@ -21,6 +21,36 @@ func main() {
 	fmt.Println()
 
 	fmt.Println("Have some Pi! ", math.Pi)
+
+	fmt.Println()
+
+	fmt.Println("I'm adding 43 + 55 = ", add(43, 55))
+
+	fmt.Println()
+
+	a, b := swap("Rock", "You")
+	fmt.Println("a, b := swap(\"Rock\", \"You\")")
+	fmt.Println("I'm swapping these out, Rock You ! ", a, b, "!")
+
+	fmt.Println()
+
+	//fmt.Println("Named Return example via func nakedreturnsplit(17), ", nakedreturnsplit(17))
+	fmt.Println(nakedreturnsplit(17))
+
+}
+
+func nakedreturnsplit(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func add(x, y int) int {
+	return x + y
 }
 
 func sqrttest(f float64) {
