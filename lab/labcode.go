84a73731c44c368239f7e02b8c31bff0086e5c96
jreidell <joe@reidell.net>
2019-02-10 13:53:39 -0500

    Working on Tour of Go via the Lab project

--- lab/labcode.go
+++ lab/labcode.go
@@ -1,13 +1,30 @@
+// Lab is a collection of Tour of Go examples and learning excercises
 package main
 
 import (
 	"fmt"
 	"math"
+	"math/cmplx"
 	"math/rand"
 	"time"
 )
 
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
 func main() {
+
+	python, c, java := true, false, "no!"
+	k := 3
+	var i, j int = 1, 2
+
+	fmt.Println("Various Var Declaration Example: ", k, python, c, java, j, i)
+
+	fmt.Println()
+
 	timetest()
 
 	fmt.Println()
@@ -28,21 +45,51 @@ func main() {
 
 	fmt.Println()
 
-	a, b := swap("Rock", "You")
-	fmt.Println("a, b := swap(\"Rock\", \"You\")")
-	fmt.Println("I'm swapping these out, Rock You ! ", a, b, "!")
+	writeswapresult()
 
 	fmt.Println()
 
 	//fmt.Println("Named Return example via func nakedreturnsplit(17), ", nakedreturnsplit(17))
 	fmt.Println(nakedreturnsplit(17))
 
+	fmt.Println()
+
+	cmplxmath()
+
+	fmt.Println()
+
+	defaultvalues()
+}
+
+func defaultvalues() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
+
+func cmplxmath() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
 
 func nakedreturnsplit(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
 	return
+}
+
+func writeswapresult() {
+	a, b := swap("Rock", "You")
+
+	fmt.Println("a, b := swap(\"Rock\", \"You\")")
+
+	fmt.Println("I'm swapping these out, Rock You ! ", a, b, "!")
 }
 
 func swap(x, y string) (string, string) {
