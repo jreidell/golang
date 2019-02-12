a23263be8f8eea5efd440cafccbfd9621d460364
jreidell <joe@reidell.net>
2019-02-12 12:51:10 -0500

    Added basic and numeric constant demos

--- lab/labcode.go
+++ lab/labcode.go
@@ -23,19 +23,11 @@ func main() {
 
 	fmt.Println("Various Var Declaration Example: ", k, python, c, java, j, i)
 
-	fmt.Println()
-
 	timetest()
-
-	fmt.Println()
 
 	randtest()
 
-	fmt.Println()
-
 	sqrttest(7)
-
-	fmt.Println()
 
 	fmt.Println("Have some Pi! ", math.Pi)
 
@@ -47,8 +39,6 @@ func main() {
 
 	writeswapresult()
 
-	fmt.Println()
-
 	//fmt.Println("Named Return example via func nakedreturnsplit(17), ", nakedreturnsplit(17))
 	fmt.Println(nakedreturnsplit(17))
 
@@ -56,27 +46,78 @@ func main() {
 
 	cmplxmath()
 
-	fmt.Println()
-
 	defaultvalues()
 
-	fmt.Println()
-
 	typeconvtest()
+
+	funwithconst()
+
+	funwithnumconst()
+}
+
+// Pi decalred for funwithconst()
+const Pi = 3.14
+
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
+
+// needInt will return a an int, used with funwithnumconst
+func needInt(x int) int {
+	return x*10 + 1
+}
+
+// needFloat will return a float from an int, used with funwithnumconst
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
+// funwithnumconst demostrates numeric constant values, depends on needInt and needFloat functions tho
+func funwithnumconst() {
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+	fmt.Println()
+}
+
+// funwithconst is to demonstrate the use of declaring constants
+func funwithconst() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+	fmt.Println()
 }
 
 // typeconvtest is meant to demonstrate type conversions in Go
 func typeconvtest() {
 
-	// var i = 42
+	//types can be inferred by the value on the right
+	v := 42                            // change me!
+	fmt.Printf("v is of type %T\n", v) // var i = 42
+
+	v1 := 3.142                          // change me!
+	fmt.Printf("v1 is of type %T\n", v1) // var i = 42
+
+	v2 := 3.142 + 0.51i                  // change me!
+	fmt.Printf("v2 is of type %T\n", v2) // var i = 42
+
 	// var f = float64(i)
 	// var u = uint(f)
 	// fmt.Println(i, f, u)
 
-	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
-	var z uint = uint(f)
-	fmt.Println(x, y, z)
+	// var x, y int = 3, 4
+	// var f float64 = math.Sqrt(float64(x*x + y*y))
+	// var z uint = uint(f)
+	// fmt.Println(x, y, z)
+
+	fmt.Println()
 }
 
 // defaultvalues demonstrates default values for specific types
@@ -87,6 +128,7 @@ func defaultvalues() {
 	var s string
 
 	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	fmt.Println()
 }
 
 // cmplxmath demonstrates complex math problems and printing them using Go formatting
@@ -96,6 +138,7 @@ func cmplxmath() {
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 
 	fmt.Printf("Type: %T Value: %v\n", z, z)
+	fmt.Println()
 }
 
 // nakedreturnsplit demonstrates a method using a naked return. return vars are named in the signature and not explicitly returned
@@ -112,6 +155,7 @@ func writeswapresult() {
 	fmt.Println("a, b := swap(\"Rock\", \"You\")")
 
 	fmt.Println("I'm swapping these out, Rock You ! ", a, b, "!")
+	fmt.Println()
 }
 
 // swap the values that are input to the method, return them in reverse order
@@ -127,6 +171,7 @@ func add(x, y int) int {
 // sqrttest math demo
 func sqrttest(f float64) {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(f))
+	fmt.Println()
 }
 
 // randtest random number gen demo
@@ -134,6 +179,7 @@ func randtest() {
 	fmt.Println("Have some random number yo!")
 
 	fmt.Println("The number is now ", rand.Intn(time.Now().Nanosecond()))
+	fmt.Println()
 }
 
 // timetest deomonstrates getting the current system time
@@ -141,4 +187,5 @@ func timetest() {
 	fmt.Println("Here we are again!")
 
 	fmt.Println("The time is now ", time.Now())
+	fmt.Println()
 }
